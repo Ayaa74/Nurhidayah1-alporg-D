d5ba22a1f85d5b1f32aeb23ac2f07f44fb54e240
Ayaa74 <trijuliyanti15075@gmail.com>
2024-11-16 23:04:59 +0800

    Add files via upload

---
+++ TUGAS 6 (24241135).py
@@ -0,0 +1,18 @@
+# Modifikasi Tugas 2
+
+x = 35
+y = 24
+luas = 840
+
+x = input("masukan nilai x:")
+print("nilai x adalah :",x)
+
+y = input("masukan nilai y:")
+print("nilai y adalah :",y)
+
+luas = input("masukan nilai luas:")
+print("nilai luas adalah :",luas)
+
+
+
+
