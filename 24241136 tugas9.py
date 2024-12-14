0d9e4af253763ea6abd593c175a02c57ad74dda4
Ayaa74 <trijuliyanti15075@gmail.com>
2024-12-14 13:44:59 +0800

    Add files via upload

---
+++ 24241136 tugas9.py
@@ -0,0 +1,17 @@
+    #suhu Celcius 36 
+    # suhu dalam Fahrenheit = 75.2
+    # suhu dalam Kelvin = 297, 15
+    # suhu dalam reamur = 19, 2
+    
+    # suhu dalam Celcius 
+    Celcius 36
+    
+    # rumus konversi suhu
+    derajat Fahrenheit =(Celcius * 9/5)+
+    Kelvin = Celcius + 273,15
+    tempat tidur = Celcius * 4/5
+    
+    # cetak nilai suhu 
+    print("suhu dalam Fahrenheit:", Fahrenheit)
+    print("suhu dalam Kelvin:", Kelvin)
+    print(" suhu dalam reamur:", reamur)
