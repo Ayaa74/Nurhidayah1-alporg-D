3fd67ba3f7d8edad84186c0b3a6aee616881af31
Ayaa74 <trijuliyanti15075@gmail.com>
2025-01-04 14:45:34 +0800

    Add files via upload

---
+++ 24241136 tugas11.py
@@ -0,0 +1,30 @@
+## Logika bolean
+
+# not and or xor
+
+print("==========not==========")
+x = 2
+z = 6
+print("nilai z = ", z)
+
+#(jika ada salah, maka hasilnya salah)
+print("======== AND ========")
+x = 2
+y = 6
+nilai = x and y
+print(x, "and", y, "=", nilai)
+
+x = 2
+y = 6
+nilai = x and y
+print(x, "and", y, "=", nilai)
+
+x = 2
+y = 6
+nilai = x and y
+print(x, "and", y, "=", nilai)
+
+x = 2
+y = 6
+nilai = x and y
+print(x, "and", y, "=", nilai)
